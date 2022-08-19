fab286399e49d4213b16c538a00a3871d9ac938b
Artak1999 <dashyan.1999@mail.ru>
2022-08-19 12:49:20 +0500

    Useful task

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,11 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException{
+    if(studentID >= 1 && studentID <= 11)
+      return Student.getValueOf(studentID);
+    else
+      throw new StudentNotFoundException(studentID);
   }
 
   public static void main(String[] args) {
